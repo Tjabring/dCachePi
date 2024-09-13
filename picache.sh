c65db9d304ef1dfe1374e5d3c89dd4a402638f3d
Tjabring <tjabring.vanegten@surf.nl>
2024-09-13 02:48:24 +0200

    added avahi-daemon and adjusted some tests

--- picache.sh
+++ picache.sh
@@ -413,7 +413,7 @@ fi
 echo -e '\033[32m      dCache is ready for use!\033[0m'
 echo " "
 
-echo "You can test uploading the README.md file with webdav now. Use localhost, hostname, or IP address"
+echo "You can test uploading the README.md file with webdav now. Use localhost, hostname or IP address."
 echo "curl -v -u tester:$PASSWD -L -T README.md http://localhost:2880/home/tester/README.md"
 echo " "
 
@@ -421,13 +421,13 @@ echo "You can check the upload with a curl PROPFIND command."
 echo "curl -s -u tester:dcache123 -X PROPFIND http://localhost:2880/home/tester/ | xmlstarlet sel -N d=\"DAV:\" -t -m \"//d:response\" -v \"concat(d:href, ' ', d:propstat/d:prop/d:displayname, ' ', d:propstat/d:prop/d:getlastmodified)\" -n"
 echo " "
 
-echo "You can test xrootd / xrdcp"
+echo "You can test xrootd / xrdcp."
 echo "xrdcp -f /bin/bash root://localhost:1096/home/tester/testfile # upload"
 echo "xrdcp -f root://localhost:1096/home/tester/testfile /tmp/testfile # download"
 echo " "
 
 testfilestamp=$(date +%s)
-echo "You can test dcap / dccp"
+echo "You can test dcap / dccp."
 echo "dccp -A /bin/bash dcap://localhost:22125/home/tester/testfile-$testfilestamp"
 echo "dccp -A dcap://localhost:22125/home/tester/testfile-$testfilestamp /tmp/testfile-$testfilestamp"
 echo " "
@@ -437,6 +437,6 @@ echo "Admin console: ssh -p 22224 admin@localhost # with your provided password 
 echo " "
 
 ETH0_IP=$(ip -4 addr show dev eth0 | grep inet | awk '{print $2}' | cut -d/ -f1)
-echo "You can also access the web interface - dCache service"
+echo "You can also access the web interface - dCache service."
 echo "http://${HOSTNAME}.local:2288 or http://${ETH0_IP}:2288"
 
